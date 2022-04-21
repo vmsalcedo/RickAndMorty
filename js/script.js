166598e631e0fbb32664a26bfa487e9b583892a9
vmsalcedo <vm.salcedo@hotmail.com>
2022-04-21 00:41:45 -0500

    Modificando tienda

--- js/script.js
+++ js/script.js
@@ -7,32 +7,34 @@ const IGV = 1.18
 const precioPoleras = 10
 const precioPijamas = 20
 
+
+//Funcion para precio de Poleras
 function precioTotal (num1) {
     return (num1 * precioPoleras) * (IGV)
 }
 
-
+//Funcion para precio de Pijamas
 function precioTotal2 (num1) {
     return (num1 * precioPijamas) * (IGV)
 }
 
 
+function tresErrores (error){
+    return error + error++
+}
 
 
 
 let producto;
 let valor = true;
 while (valor) {
-    producto = prompt("Ingresa el Producto que deseas : Poleras y Pijamas");
-    producto = producto.toLowerCase(); //convierto variable a minusculas //
-    if (producto === "poleras") {
-        valor = false; // si es V entonces termina el ciclo //
+    producto = prompt("Ingresa el Producto que deseas : Poleras y Pijamas").toLowerCase();
+    if (producto === "poleras" || producto==="polera") {
+        valor = false; // si es V entonces termina el ciclo //  
 
-       
-
-    } else if (producto === "pijamas") {
+    } else if (producto === "pijamas"|| producto==="pijama") {
         valor = false; // si es V entonces termina el ciclo //
-    } else if (producto == null) {
+    } else if (producto === null) {
         break;
     } else {
         alert("Lo siento no tenemos ese Producto, gracias");
@@ -44,17 +46,17 @@ while (valor) {
 let color;
 let valor1 = true;
 while (valor1) {
-    color = prompt("Tenemos dos Colores: Blanco - Negro");
-    color = color.toLowerCase(); //convierto variable a minusculas //
-    if (color == "blanco") {
+    color = prompt("Tenemos dos Colores: Blanco - Negro").toLowerCase();
+    
+    if (color === "blanco") {
         valor1 = false; // si es V entonces termina el ciclo //
 
     
         
 
-    } else if (color == "negro") {
+    } else if (color === "negro") {
         valor1 = false; // si es V entonces termina el ciclo //
-    } else if (color == null) {
+    } else if (color === null) {
         break;
     } else {
         alert("Lo siento no tenemos ese Color, gracias");
@@ -65,14 +67,14 @@ while (valor1) {
 let talla;
 let valor2= true;
 while (valor2){
-    talla = prompt ("Tenemos Tallas: M y talla L")
-    talla = talla.toLowerCase();
-    if (talla == "m") {
+    talla = prompt ("Tenemos Tallas: M y talla L").toLowerCase();
+   
+    if (talla === "m") {
         valor2 = false; // si es V entonces termina el ciclo //    
 
-    } else if (talla == "l") {
+    } else if (talla === "l") {
         valor2 = false; // si es V entonces termina el ciclo //
-    } else if (talla == null) {
+    } else if (talla === null) {
         break;
     } else {
         alert("Lo siento no tenemos esa talla, gracias");
@@ -104,20 +106,15 @@ while (valor3) {
 
 
 
-
-
-
-
-
-if (producto==="poleras"){
+if (producto==="poleras" || producto==="polera"){
     let precioFinal =precioTotal(cantidad)
-    alert("El precio por "+ cantidad +" Poleras "+"es: S/. " +precioFinal.toFixed(2) +" Incluido impuestos")
+    alert("El precio por "+ cantidad +" Polera (as) "+"es: S/. " +precioFinal.toFixed(2) +" Incluido impuestos")
 }
 
 
-else if (producto==="pijamas"){
+else if (producto==="pijamas"|| producto==="pijama"){
     let precioFinal =precioTotal2(cantidad)
-    alert("El precio por "+ cantidad +" Pijamas "+"es: S/. " +precioFinal.toFixed(2) +" Incluido impuestos")
+    alert("El precio por "+ cantidad +" Pijama (as) "+"es: S/. " +precioFinal.toFixed(2) +" Incluido impuestos")
 }
 
 
